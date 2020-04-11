9621b14c0a3e11c47d92eb000edd1d6546302e08
Dennis Ploeger <dennis.ploeger@kps.com>
2020-04-11 21:28:18 +0200

    feat: Search feature test

---
+++ cypress/integration/search_spec.ts
@@ -0,0 +1,19 @@
+describe('The search feature', () => {
+  beforeEach(function() {
+    cy.login()
+    cy.visit(`https://web:8443/search?q=${this.testdata.search.term}&search_type=all`)
+  })
+  it('should return a minimum set of results', function () {
+    cy.wrap(Object.keys(this.counts.search.minResults))
+      .each(
+        (selector, index, collection) => {
+          cy.get(selector).
+            then(
+              elem => {
+                expect(parseInt(elem.text())).to.be.at.least(parseInt(this.counts.search.minResults[selector]))
+              }
+          )
+        }
+      )
+  })
+})
